a97729801d005a2843245e6629e7520ea478264c
vednolacni <123391433+vednolacni@users.noreply.github.com>
2026-02-22 20:28:00 +0100

    Add database migration to start script
    Added database migration command before starting the service.

--- start.sh
+++ start.sh
@@ -6,4 +6,7 @@ PORT="${PORT:-9000}"
 # zamenjaj address port v configu
 sed -i "s/address = \".*\"/address = \"0.0.0.0:${PORT}\"/g" /listmonk/config.toml
 
+# database migration
+./listmonk --upgrade --yes --config /listmonk/config.toml
+
 exec ./listmonk --config /listmonk/config.toml
